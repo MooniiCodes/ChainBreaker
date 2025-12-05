0fc300b5074f131452ea8af314d15473913a4e0a
MrMoonii <alexandrobertoyt@gmail.com>
2025-12-05 16:20:20 -0600

    Create sh.py

---
+++ jb/lib/sh.py
@@ -0,0 +1,8 @@
+class shtools:
+  def shexec(cmd):
+    if cmd == "exit":
+      pass # Do some import stuff later
+    elif cmd == "test":
+      print("Test works!")
+    else:
+      print("Invalid command.")
