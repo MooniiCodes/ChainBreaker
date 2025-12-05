41d5b2cb06c4533a1a4dd1c9b4c15ededab73c98
MrMoonii <alexandrobertoyt@gmail.com>
2025-12-05 16:41:40 -0600

    Create moon.py

---
+++ moon.py
@@ -0,0 +1,27 @@
+# Functions
+
+def showAlert(title, message):
+  print(f"\n=|{title}|=")
+  print("==========================")
+  print(message)
+  print("==========================\n")
+
+# Configuration
+version = 1.01
+safetyLock = False
+# Loader
+print("Press EXE 3 times.")
+input()
+input()
+input()
+print(f"MoonLoader {version}\n")
+while True:
+  i = input("> ")
+  if i.lower() == "e" or i.lower() == "exit":
+    break
+  elif i.lower() == "hw":
+    print("Hello, world!")
+  elif i.lower() == "example":
+    showAlert("Example", "This is a example alert.")
+  else:
+    print("Invalid command.")
